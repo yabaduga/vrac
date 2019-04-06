05fbf39e9e59abdb2cfbaea22a83a28f07ad828b
manuel@antunes.me <manuel@antunes.me>
2019-04-06 16:07:36 +0200

    final // création de constante par constructeur initialisation attribut à la décla

---
+++ FR_178_05_03/src/Principale.java
@@ -0,0 +1,9 @@
+public class Principale {
+    static final int tva20=20;
+    public static void main (String[] args){
+
+
+
+    }
+
+}
---
+++ FR_178_05_03/src/TestVariableFinal.java
@@ -0,0 +1,9 @@
+public class TestVariableFinal {
+    final int variable =6;
+    TestVariableFinal(){
+        // variable=19;
+    }
+    void methode (){
+        variable = 8;
+    }
+}
