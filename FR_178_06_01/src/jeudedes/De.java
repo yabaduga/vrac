81104f6cfec3ca941ce13ee2a706e793253f3547
manuel@antunes.me <manuel@antunes.me>
2019-04-06 18:32:32 +0200

    mise en place random sur lancer de dés ajout/modification boucle tests

--- FR_178_06_01/src/jeudedes/De.java
+++ FR_178_06_01/src/jeudedes/De.java
@@ -1,6 +1,19 @@
 package jeudedes;
 public class De {
 private int valeurFace;
+public int rouler() {
+    int j=0, min=1, max =6, range =max-min+1;
+   valeurFace=(int)(Math.random()*range+min);
+    //valeurFace=(int)(Math.random()*6+1);
+    if (valeurFace>5) {
+
+        System.out.println("la valeur nominale " + valeurFace);
+
+    }
+    //valeurFace=4;
+    return valeurFace;
+}
+
 
     public int getValeur() {
         return valeurFace;
