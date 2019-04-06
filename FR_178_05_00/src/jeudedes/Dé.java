8046d5446188d4b56873e2becaa6a71b4537137a
manuel@antunes.me <manuel@antunes.me>
2019-04-06 11:29:37 +0200

    primitive et déclaration d'un champ de donnée initialisation à la déclaration ou via constructeur vid1

---
+++ FR_178_05_00/src/jeudedes/Dé.java
@@ -0,0 +1,13 @@
+package jeudedes;
+
+public class Dé {
+
+    private int valeurDe;
+
+    public int getValeur() {
+        return valeurDe;
+    }
+public Dé(){
+        valeurDe=5;
+}
+}
---
+++ FR_178_05_00/src/jeudedes/Principale.java
@@ -0,0 +1,9 @@
+package jeudedes;
+
+public class Principale {
+    public static void main (String [] args) {
+        Dé d=new Dé();
+
+        System.out.println("Le dé vaut "+d.getValeur());
+    }
+}
